2b2b560b6eb16e14db905e452d78b5e6633ce2ea
Denis Oliveira <denisdev3loper@gmail.com>
2025-09-02 22:57:35 -0300

    update

--- src/App.vue
+++ src/App.vue
@@ -6,23 +6,4 @@
 </script>
 
 <style scoped>
-.layout {
-  display: flex;
-  height: 100vh;
-  width: 100%;
-  background: #f9fafb;
-}
-
-.content {
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-  margin-left: 260px; /* largura da sidebar */
-}
-
-.main-content {
-  flex: 1;
-  padding: 1.5rem;
-  overflow-y: auto;
-}
 </style>
